c2ac9153a4c9b8e9c8d4b4a0fe01ef57183366ab
MedLaaribi <mohamed.laaribi.dev@gmail.com>
2024-05-29 19:45:48 +0200

    Camp and Guide

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,55 +1,89 @@
-import Image from 'next/image'
-import React from 'react'
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
 
-const Guide = () => {
+interface CampProps {
+    backgroundImage: string;
+    title: string;
+    subtitle: string;
+    peopleJoined: string;
+}
+
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
     return (
-        <section className="flexCenter flex-col">
-            <div className="padding-container max-container w-full pb-24">
-                <Image src="/camp.svg" alt="camp" width={50} height={50} />
-                <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
-                    We are here for you
-                </p>
-                <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
-                    <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Guide You to Easy Path</h2>
-                    <p className="regular-16 text-gray-30 xl:max-w-[520px]">Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+        <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+            <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+                <div className="flexCenter gap-4">
+                    <div className="rounded-full bg-green-50 p-4">
+                        <Image
+                            src="/folded-map.svg"
+                            alt="map"
+                            width={28}
+                            height={28}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h4 className="bold-18 text-white">{title}</h4>
+                        <p className="regular-14 text-white">{subtitle}</p>
+                    </div>
+                </div>
+
+                <div className="flexCenter gap-6">
+        <span className="flex -space-x-4 overflow-hidden">
+          {PEOPLE_URL.map((url) => (
+              <Image
+                  className="inline-block h-10 w-10 rounded-full"
+                  src={url}
+                  key={url}
+                  alt="person"
+                  width={52}
+                  height={52}
+              />
+          ))}
+        </span>
+                    <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
                 </div>
             </div>
+        </div>
+    )
+}
 
-            <div className="flexCenter max-container relative w-full">
-                <Image
-                    src="/boat.png"
-                    alt="boat"
-                    width={1440}
-                    height={580}
-                    className="w-full object-cover object-center 2xl:rounded-5xl"
+const Camp = () => {
+    return (
+        <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+            <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+                <CampSite
+                    backgroundImage="bg-bg-img-1"
+                    title="Putuk Truno Camp"
+                    subtitle="Prigen, Pasuruan"
+                    peopleJoined="50+ Joined"
                 />
+                <CampSite
+                    backgroundImage="bg-bg-img-2"
+                    title="Mountain View Camp"
+                    subtitle="Somewhere in the Wilderness"
+                    peopleJoined="50+ Joined"
+                />
+            </div>
 
-                <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+            <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+                <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+                    <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+                        <strong>Feeling Lost</strong> And Not Knowing The Way?
+                    </h2>
+                    <p className="regular-14 xl:regular-16 mt-5 text-white">
+                        Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+                    </p>
                     <Image
-                        src="/meter.svg"
-                        alt="meter"
-                        width={16}
-                        height={158}
-                        className="h-full w-auto"
+                        src="/quote.svg"
+                        alt="camp-2"
+                        width={186}
+                        height={219}
+                        className="camp-quote"
                     />
-                    <div className="flexBetween flex-col">
-                        <div className='flex w-full flex-col'>
-                            <div className="flexBetween w-full">
-                                <p className="regular-16 text-gray-20">Destination</p>
-                                <p className="bold-16 text-green-50">48 min</p>
-                            </div>
-                            <p className="bold-20 mt-2">Aguas Calientes</p>
-                        </div>
-
-                        <div className='flex w-full flex-col'>
-                            <p className="regular-16 text-gray-20">Start track</p>
-                            <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
     )
 }
 
-export default Guide
+export default Camp

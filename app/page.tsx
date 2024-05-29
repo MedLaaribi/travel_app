daeda94582862104a41c265eeb847be76aaf4712
MedLaaribi <mohamed.laaribi.dev@gmail.com>
2024-05-29 16:59:09 +0200

    components update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 
 export default function Home() {
   return (
-      <h1 className="font-bold text-teal-500">
-        Hello World
-      </h1>
+      <>
+          <Hero />
+          <Camp/>
+          <Guide/>
+          <Features/>
+          <GetApp />
+
+      </>
 
   );
 }
